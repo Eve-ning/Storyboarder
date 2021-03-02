3ba92f4d06e1fde2ea5bae2518f2bf1e0f811c39
Evening <johnchangqi@hotmail.com>
2021-03-03 03:46:52 +0800

    Update required test

--- TimelineHandlerUT/TimelineHandlerUT.cs
+++ TimelineHandlerUT/TimelineHandlerUT.cs
@@ -2,56 +2,57 @@ using System;
 using System.Collections.Generic;
 using EventHandler.Modifiers;
 using EventHandler.Sprite;
+using EventHandler.Tools;
 using NUnit.Framework;
 using TimelineHandler.Timeline;
 
 namespace TimelineHandlerUT
 {
     using EvCon = SpriteEventConstructor;
-    using TlCon = TlSpriteEventConstructor;
+
     public class Tests
     {
+        private String dir = "tests/TlEventHandlerTests/";
         private int _pts = 100;
-        private List<EvCon> _evCon = new List<EvCon>();
-        private TlCon _tlCon;
-        private List<float> begins = new List<float>() {1000f, 3000f};
-        private List<float> ends = new List<float>() {2000f, 4000f};
+        private TlSpriteEventList _tlCon;
         private double delta = 0.0001f;
+        
+        public void PlotPoints(SpriteEventList eventList)
+        {
+            EventPlotter.PlotPoints(eventList,
+                dir +
+                (new System.Diagnostics.StackTrace()).GetFrame(1)?.GetMethod()?.Name +
+                ".png");
+        }
         
         [SetUp]
         public void Setup()
         {
-            var evCon = new EvCon(begins[0], ends[0]);
-            evCon.Modifiers.Add(new EventRotate((float) Math.PI));
-            _evCon.Add(evCon);
-            
-            evCon = new EvCon(begins[1], ends[1]);
-            evCon.Modifiers.Add(new EventScaleY(0.5f));
-            _evCon.Add(evCon);
-
-            _tlCon = new TlSpriteEventConstructor(_evCon);
+            System.IO.Directory.CreateDirectory(dir);
         }
 
         [Test]
-        public void TlSampler()
+        public void TestTlWorkflow()
         {
-            var samples = _tlCon.Sample(_pts);
-            
-            Assert.AreEqual(begins[0],samples.T[0]);
-            Assert.AreEqual(ends[0],  samples.T[_pts]);
-            Assert.AreEqual(begins[1],samples.T[_pts+1]);
-            Assert.AreEqual(ends[1],  samples.T[_pts*2+1]);
-            
-            Assert.AreEqual(0, samples.X[0],        delta);
-            Assert.AreEqual(0, samples.X[_pts],     delta);
-            Assert.AreEqual(0, samples.X[_pts+1],   delta);
-            Assert.AreEqual(0, samples.X[_pts*2+1], delta);
-            
-            Assert.AreEqual(-1,  samples.Y[0],        delta);
-            Assert.AreEqual(0,   samples.Y[_pts],     delta);
-            Assert.AreEqual(0.5, samples.Y[_pts+1],   delta);
-            Assert.AreEqual(0,   samples.Y[_pts*2+1], delta);
-            
+            var constructor = new SpriteEventConstructor();
+            var sampleOut = TlSpriteEventList.Join(new List<SpriteEventList>()
+            {
+                constructor
+                    .SampleEvents(_pts)
+                    .Modify
+                    .Rotate(t => (t + 1) * (float) Math.PI)
+                    .ScaleX(0.25f).ScaleY(0.25f)
+                    .TimeRange(2000, 3000).EventList,
+
+                constructor
+                    .SampleEvents(_pts)
+                    .Modify
+                    .Rotate(t => (t + 1) * (float) Math.PI)
+                    .ScaleX(0.75f).ScaleY(0.75f)
+                    .TimeRange(1000, 2000).EventList
+            });
+            sampleOut.Modify.Rotate(0.25f * (float) Math.PI);
+            PlotPoints(sampleOut);
         }
 
         
