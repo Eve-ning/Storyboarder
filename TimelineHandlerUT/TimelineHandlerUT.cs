8c1d9afea35879ae3cd7e12c7e4798c58d49e2d0
Evening <johnchangqi@hotmail.com>
2021-03-03 15:48:09 +0800

    Update Test for Align Rotation

--- TimelineHandlerUT/TimelineHandlerUT.cs
+++ TimelineHandlerUT/TimelineHandlerUT.cs
@@ -6,55 +6,54 @@ using EventHandler.Tools;
 using NUnit.Framework;
 using TimelineHandler.Timeline;
 
-namespace TimelineHandlerUT
-{
+namespace TimelineHandlerUT {
     using EvCon = SpriteEventConstructor;
 
-    public class Tests
-    {
+    public class Tests {
         private String dir = "tests/TlEventHandlerTests/";
         private int _pts = 100;
         private TlSpriteEventList _tlCon;
         private double delta = 0.0001f;
-        
-        public void PlotPoints(SpriteEventList eventList)
-        {
+
+        public void PlotPoints(SpriteEventList eventList) {
             EventPlotter.PlotPoints(eventList,
                 dir +
                 (new System.Diagnostics.StackTrace()).GetFrame(1)?.GetMethod()?.Name +
                 ".png");
         }
-        
+
         [SetUp]
-        public void Setup()
-        {
+        public void Setup() {
             System.IO.Directory.CreateDirectory(dir);
         }
 
         [Test]
-        public void TestTlWorkflow()
-        {
-            var constructor = new SpriteEventConstructor();
-            var sampleOut = TlSpriteEventList.Join(new List<SpriteEventList>()
-            {
-                constructor
-                    .SampleEvents(_pts)
+        public void TestTlWorkflowCircle() {
+            var constructor = new SpriteEventConstructor(yBegin: 0, tBegin:0, tEnd:2 * (float)Math.PI);
+            var sampleOut = TlSpriteEventList.Join(new List<SpriteEventList>() {
+                constructor.SampleEvents(_pts)
                     .Modify
-                    .Rotate(t => (t + 1) * (float) Math.PI)
-                    .ScaleX(0.25f).ScaleY(0.25f)
-                    .TimeRange(2000, 3000).EventList,
-
-                constructor
-                    .SampleEvents(_pts)
-                    .Modify
-                    .Rotate(t => (t + 1) * (float) Math.PI)
-                    .ScaleX(0.75f).ScaleY(0.75f)
-                    .TimeRange(1000, 2000).EventList
-            });
-            sampleOut.Modify.Rotate(0.25f * (float) Math.PI);
+                    .SetX(t => (float) Math.Cos(t))
+                    .SetY(t => (float) Math.Sin(t))
+                    .SetTimeRange(1000, 2000)
+                    .AlignRotate()
+                    .EventList,
+            }, false);
             PlotPoints(sampleOut);
+            Assert.AreEqual((float)   Math.PI / 2,sampleOut.R[0],            delta);
+            Assert.AreEqual(0,                    sampleOut.R[_pts / 4],     delta);
+            Assert.AreEqual((float) - Math.PI / 2,sampleOut.R[_pts / 2],     delta);
+            Assert.AreEqual((float)   Math.PI,    sampleOut.R[_pts / 4 * 3], delta);
+            
+            Assert.AreEqual(1 ,sampleOut.X[0],            delta);
+            Assert.AreEqual(0 ,sampleOut.X[_pts / 4],     delta);
+            Assert.AreEqual(-1,sampleOut.X[_pts / 2],     delta);
+            Assert.AreEqual(0 ,sampleOut.X[_pts / 4 * 3], delta);
+            
+            Assert.AreEqual(0 ,sampleOut.Y[0],            delta);
+            Assert.AreEqual(1 ,sampleOut.Y[_pts / 4],     delta);
+            Assert.AreEqual(0 ,sampleOut.Y[_pts / 2],     delta);
+            Assert.AreEqual(-1,sampleOut.Y[_pts / 4 * 3], delta);
         }
-
-        
     }
 }
